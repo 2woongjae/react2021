2b1e53b07f0eb0b2d889cfaec3badb50e6df5019
Mark Lee <mark@protopie.io>
2021-06-07 22:48:41 +0900

    style-loaders-example

---
+++ style-loaders-example/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+// import './App.css';
+// import "./App.scss";
+import styles from "./App.module.css";
+
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className={styles["App"]}>
+      <header className={styles["header"]}>
+        <img src={logo} className={styles["logo"]} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button>Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
