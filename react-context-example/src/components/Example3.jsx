3d6262b0c578e8f128b770258da3443a8d210178
Mark Lee <mark@protopie.io>
2021-06-07 22:47:10 +0900

    react-context-example

---
+++ react-context-example/src/components/Example3.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import PersonContext from "../contexts/PersonContext";
+
+export default function Example3() {
+  const persons = useContext(PersonContext);
+
+  return (
+    <ul>
+      {persons.map((person) => (
+        <li>{person.name}</li>
+      ))}
+    </ul>
+  );
+}
